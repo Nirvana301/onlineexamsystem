385d4cf58fa6601e62551c4a84b819d82a2ee00b
SBaranH <36442894+SBaranH@users.noreply.github.com>
2021-01-07 21:24:57 +0300

    Demo

--- OExam-Part 1/OnlineExam/resources/views/email/reset_mail.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Hello, {{$user->user_name}} </h1>
-<p>
-    Please click the <a href="{{ url('reset_password/'.$user->email.'/'.$code) }}" onclick="false">Reset Password</a> link to reset your password.
-</p>
-<p>(If 'Reset Password' link is not clickable, mark this email as 'Not Spam'.)
-</p>
-
-
